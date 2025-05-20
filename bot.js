321225ebde33b12f26012f3008ffda4a4e4c9ccb
javad Goldoust <jgoldoost@gmail.com>
2025-05-20 19:57:44 +0330

    second commit

--- bot.js
+++ bot.js
@@ -1,7 +1,9 @@
-require('dotenv').config();
 const { Telegraf } = require('telegraf');
+const express = require('express');
+require('dotenv').config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const app = express();
 
 bot.start((ctx) => ctx.reply('سلام! خوش اومدی 👋'));
 bot.help((ctx) => ctx.reply('دستوراتی که می‌تونی استفاده کنی: /start /help'));
@@ -9,5 +11,13 @@ bot.on('text', (ctx) => {
   ctx.reply(`تو گفتی: ${ctx.message.text}`);
 });
 
-bot.launch();
-console.log('ربات راه افتاد ✅');
+// استفاده از webhook
+app.use(bot.webhookCallback('/secret-path'));
+
+// راه‌اندازی webhook در Telegraf
+bot.telegram.setWebhook(`https://${process.env.RENDER_EXTERNAL_URL}/secret-path`);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`ربات روی پورت ${PORT} اجرا شد 🚀`);
+});
